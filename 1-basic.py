0520105aa5ebdb64c356f5f451e7faf521dfd4c7
zw84808961 <zw84808961@gmail.com>
2016-02-02 16:25:23 +0800

    Rename and Create files

--- 1-basic.py
+++ 1-basic.py
@@ -1,6 +1,16 @@
 # coding=utf-8
+#多行语句
 
-# Python 引号
+total = 'item_one' + ' '\
+		'item_two' + ' '\
+		'item_three'
+print(total)
+#语句中包含[], {} 或 () 括号就不需要使用多行连接符	
+days = ['Monday','Tuesday','Wednesday',
+		'Thursday','Friday']
+
+
+# Python 接收单引号(' )，双引号(" )，三引号(''' """) 来表示字符串
 word = 'word'
 sentence = "这是一个句子"
 paragraph = """这是一个段落
@@ -26,6 +36,7 @@ print(paragraph)
 #等待用户输入
 aaa = raw_input("\n\nPress the enter key to exit.")
 print aaa
-#Python可以在同一行中使用多条语句，语句之间使用分号(;)分割，以下是一个简单的实例：
 
+
+#Python可以在同一行中使用多条语句，语句之间使用分号(;)分割，以下是一个简单的实例：
 import sys; x = 'foo'; sys.stdout.write(x + '\n')
